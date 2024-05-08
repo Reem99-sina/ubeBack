b5c4f80b057d882401cd00904104621b7ffc77ad
Reem99-sina <reemsina2@gmail.com>
2024-05-09 02:02:19 +0300

    add user

--- module/user.js
+++ module/user.js
@@ -9,6 +9,7 @@ phoneNumber:{type:String, unique: true},
 activeStatus:String,
 role:String,
 driver_id:{type:mongoose.Types.ObjectId,ref:"user"},
-active_status:Boolean
+active_status:Boolean,
+location:{lat:String,long:String}
 })
 module.exports.User = mongoose.model('user', userSchema);

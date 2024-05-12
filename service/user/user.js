011d6685045da29d10bdf5f0442c5c065b303882
Reem99-sina <reemsina2@gmail.com>
2024-05-12 19:13:36 +0300

    driver

--- service/user/user.js
+++ service/user/user.js
@@ -2,6 +2,7 @@ const { Vonage } = require("@vonage/server-sdk");
 const { User } = require("../../module/user");
 const sendEmail = require("../../utils/sendEmail");
 const { SMS } = require("@vonage/messages");
+const { default: axios } = require("axios");
 
 const vonage = new Vonage({
   apiKey: "9430c4d3",
@@ -62,24 +63,29 @@ const updateDriver = async (req, res) => {
 const sendOtp = async (req, res) => {
   const RandomNumber = Math.ceil(Math.random() * 1000);
   const UserEmail = await User.findOne({ phoneNumber: req.body.to });
-  await vonage.messages.send(new SMS(
-      `${req.body.text}  code:${RandomNumber}`,
-       req.body.to,
-       req.body.from
-    ))
-    .then((resp) => {
-      console.log(resp,"resp");
-      res
-        .status(200)
-        .json({
-          message: "Message sent successfully",
-          code: RandomNumber,
-          user: UserEmail,
-        });
-    })
-    .catch((err) => {
-      res.status(400).json({ message: err });
-    });
+  axios.post("https://k2rgd3.api.infobip.com/sms/2/text/advanced",{
+    messages: [
+      {
+          destinations: [{to:req.body.to}],
+          from: "ServiceSMS",
+          text: `Congratulations on sending your first message.\nGo ahead and check the delivery report in the next step ${RandomNumber}.`
+      }
+  ]
+  },{headers:{Authorization:"App 56f0300fae690d9a005431f6228e00bf-a207de37-5f8f-47d4-8945-a994eeb6e4e9"}}).then((ressult)=> res
+  .status(200)
+  .json({message:ressult})).catch((error)=>res.status(400).json({ message: error }))
+  // await vonage.messages.send(new SMS(
+  //     `${req.body.text}  code:${RandomNumber}`,
+  //      req.body.to,
+  //      req.body.from
+  //   ))
+  //   .then((resp) => {
+  //     console.log(resp,"resp");
+     
+  //   })
+  //   .catch((err) => {
+  //     res.status(400).json({ message: err })
+  //   });
 };
 const getOtpEmail = async (req, res) => {
   const code = Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
